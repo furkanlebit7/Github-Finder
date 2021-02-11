26c0c82af546eb6337c227e3af9f64cd829a5189
furkanlebit7 <furkanlebitt7@gmail.com>
2021-02-11 12:28:11 +0300

    UserDetailjs and Searchjs updated

--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -1,85 +1,79 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import Loading from "./Loading";
 import Repos from "./Repos";
 
-class UserDetail extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
-  }
-  render() {
-    const { loading, repos } = this.props;
-    const {
-      name,
-      avatar_url,
-      location,
-      html_url,
-      bio,
-      blog,
-      followers,
-      following,
-      public_repos,
-    } = this.props.user;
-    if (loading) return <Loading />;
+const UserDetail = ({ getUser, getUserRepos, match, loading, repos, user }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+  }, []);
+  const {
+    name,
+    avatar_url,
+    location,
+    html_url,
+    bio,
+    blog,
+    followers,
+    following,
+    public_repos,
+  } = user;
+  if (loading) return <Loading />;
 
-    return (
-      <div className="container mt-3">
-        <div className="row">
-          <div className="col-md-3">
-            <div className="card">
-              <img src={avatar_url} className="card-img-top" />
-              <div className="card-body">
-                <p className="card-text">{name}</p>
-                <p>
-                  <i className="fas fa-map-marker-alt">{location}</i>
-                </p>
-                <p>
-                  <a
-                    className="btn btn-primary btn-block btn-sm"
-                    href={html_url}
-                  >
-                    Github Profile
-                  </a>
-                </p>
-              </div>
+  return (
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-3">
+          <div className="card">
+            <img src={avatar_url} className="card-img-top" />
+            <div className="card-body">
+              <p className="card-text">{name}</p>
+              <p>
+                <i className="fas fa-map-marker-alt">{location}</i>
+              </p>
+              <p>
+                <a className="btn btn-primary btn-block btn-sm" href={html_url}>
+                  Github Profile
+                </a>
+              </p>
             </div>
-          </div>
-          <div className="col-md-9">
-            <div className="card">
-              <div className="card-body">
-                {bio && (
-                  <>
-                    <h3>About</h3>
-                    <p>{bio}</p>
-                  </>
-                )}
-                {blog && (
-                  <>
-                    <h3>Blog</h3>
-                    <p>{blog}</p>
-                  </>
-                )}
-                <div>
-                  <span className="badge badge-primary m-1">
-                    Followers : {followers}
-                  </span>
-                  <span className="badge badge-danger m-1">
-                    Following : {following}
-                  </span>
-                  <span className="badge badge-success m-1">
-                    Repos : {public_repos}
-                  </span>
-                </div>
-              </div>
-            </div>
-            <ul className="list-group list-group-flush">
-              <Repos repos={repos} />
-            </ul>
           </div>
         </div>
+        <div className="col-md-9">
+          <div className="card">
+            <div className="card-body">
+              {bio && (
+                <>
+                  <h3>About</h3>
+                  <p>{bio}</p>
+                </>
+              )}
+              {blog && (
+                <>
+                  <h3>Blog</h3>
+                  <p>{blog}</p>
+                </>
+              )}
+              <div>
+                <span className="badge badge-primary m-1">
+                  Followers : {followers}
+                </span>
+                <span className="badge badge-danger m-1">
+                  Following : {following}
+                </span>
+                <span className="badge badge-success m-1">
+                  Repos : {public_repos}
+                </span>
+              </div>
+            </div>
+          </div>
+          <ul className="list-group list-group-flush">
+            <Repos repos={repos} />
+          </ul>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserDetail;
